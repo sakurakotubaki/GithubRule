cfcc32f5225ecc1c00019e93daf10f9839990602
JunichiHashimoto <gitonly543@gmail.com>
2024-02-16 06:55:10 +0900

    add: text

--- js_basic/arr.js
+++ js_basic/arr.js
@@ -1,11 +1,7 @@
-let item = ["Kboy", "Jboy", "Nboy"];
-// reduceは、配列の各要素に対して、指定された関数を適用して単一の値にするメソッドです。
-item.reduce((acc, name) => {
-  console.log(acc);
-  console.log(name);
-  return acc + name;
-}, "acc");
-
-item.push("Sboy");
-console.log(item);
-// node js_basic/arr.js
+let person = [];
+person.push('minn');
+person.push('jboy');
+let p = 'yui';
+// concatで、配列を結合する
+person = person.concat(p);
+console.log(person);
